bca636b301e371ec58c95750f252685b7d29dcd7
Uzair <uzair.sheikh@techlogix.com>
2019-02-05 17:03:10 +0500

    Scrolling

--- src/Home.js
+++ src/Home.js
@@ -7,20 +7,64 @@ class Home extends Component {
             pageNumber : 1,
             pageSize : 20,
             totalProductCount : 0,
-            products :[]
+            products :[],
+            buffer : [],
+            isLoading: true,
         }
     }
     componentWillMount() {
         this.loadFirstFetch();
     }
-    loadFirstFetch(){
+    componentDidMount() {
+        document.addEventListener('scroll', this.trackScrolling);
+      }
+      
+      componentWillUnmount() {
+        document.removeEventListener('scroll', this.trackScrolling);
+      }
+      trackScrolling = () => {
+          let {pageNumber,buffer,products,isLoading} = this.state;
+          let userReachedBtm = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+          if(userReachedBtm && !isLoading){
+            console.log("scroll");
+            console.log(window.scrollY);
+            this.setState({
+                products : products.concat(buffer),
+                buffer : [],
+                pageNumber : this.state.pageNumber + 1,
+            })
+            this.loadBuffer();
+        }
+     
+      };
+      loadBuffer(){
+        console.log("bunffer");
         let {pageNumber, pageSize} = this.state;
         let baseUrl = 'http://localhost:3000';
         fetch(`${baseUrl}/products?_page=${pageNumber}&_limit=${pageSize}`)
-      .then(response => response.json())
-      .then(products => {
-          
-        this.setState({ products })
+      .then(response => {           
+        return response.json()
+      })
+      .then(buffer => {       
+        this.setState({ buffer});
+        });
+      }  
+    loadFirstFetch(){
+        console.log("loadFirstFetch");
+        let {pageNumber, pageSize} = this.state;
+        let baseUrl = 'http://localhost:3000';
+        fetch(`${baseUrl}/products?_page=${pageNumber}&_limit=${pageSize}`)
+      .then(response => {
+        this.setState({
+            totalProductCount : +response.headers.get('X-Total-Count')
+        });      
+        return response.json()
+      })
+      .then(products => {       
+        this.setState({ products,isLoading : false});
+        }).then(()=> {
+            this.setState({pageNumber : pageNumber + 1});
+            this.loadBuffer();
         });
     }
     render(){
